a3b0ad7884e64d564549288ad2a72465ceab2aeb
Renan Pinheiro <102767994+RNNSP@users.noreply.github.com>
2022-10-05 22:37:10 -0300

    SonarCloud - SAST 2

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+    from flask import Flask
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 @app.route("/")
 def pagina_inicial():
-    return "Vai Curintia"
+    return "Hello World"
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     app.run()
