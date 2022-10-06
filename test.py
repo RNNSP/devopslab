3c0291621f08ae9c50b2fa287da58bbe20f182f2
Renan Pinheiro <102767994+RNNSP@users.noreply.github.com>
2022-10-05 22:45:35 -0300

    Teste 2

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+        # -- coding: utf-8 --
 from app import app
 import unittest
 
@@ -13,6 +13,7 @@ class Test(unittest.TestCase):
     def test_requisicao(self):
         # compara o status da requisicao (precisa ser igual a 200)
         self.assertEqual(self.result.status_code, 200)
+
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
